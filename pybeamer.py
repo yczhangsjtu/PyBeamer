acddfa9f8ed94ebf844dd5061bc440da0b376cad
Yuncong Zhang <yczhangsjtu@gmail.com>
2022-04-17 14:22:32 +0800

    Update

--- pybeamer.py
+++ pybeamer.py
@@ -2,13 +2,16 @@ from contextlib import contextmanager
 from pylatex import *
 from pylatex.utils import *
 from pylatex.base_classes import Environment, Arguments, Options
+from pylatex.base_classes.containers import Fragment as _Fragment
 from .canvas import *
+
 
 @contextmanager
 def create_canvas(pic):
   canvas = Canvas()
   yield canvas
   pic.append(NoEscape(canvas.dumps()))
+
 
 class CommonEnvironmentWithUtility(Environment):
   def vspace(self, height="0.5cm"):
@@ -22,7 +25,7 @@ class CommonEnvironmentWithUtility(Environment):
 
   def hline(self):
     self.append(NoEscape(
-      "\\noindent\\makebox[\\linewidth]{\\rule{\\textwidth}{0.4pt}}"))
+        "\\noindent\\makebox[\\linewidth]{\\rule{\\textwidth}{0.4pt}}"))
 
   def pause(self):
     self.append(Command("pause"))
@@ -108,20 +111,24 @@ class Block(CommonEnvironmentWithUtility):
   def __init__(self, *, options=None, **kwargs):
     super(Block, self).__init__(options=options, **kwargs)
 
+
 class Frame(CommonEnvironmentWithUtility):
   def __init__(self, *, title=None, options=None, **kwargs):
     super(Frame, self).__init__(options=options, **kwargs)
     if title is not None:
       self.append(Command("frametitle", arguments=[title]))
 
+
 class Column(CommonEnvironmentWithUtility):
   def __init__(self, width, **kwargs):
-    self.width=width
-    super(Column, self).__init__(arguments=Arguments(NoEscape('%g\\textwidth' % width)), **kwargs)
+    self.width = width
+    super(Column, self).__init__(arguments=Arguments(
+        NoEscape('%g\\textwidth' % width)), **kwargs)
+
 
 class Columns(Environment):
   def __init__(self, widths=None, **kwargs):
-    self.widths=widths
+    self.widths = widths
     super(Columns, self).__init__(**kwargs)
     if widths is None:
       widths = [0.5, 0.5]
@@ -139,8 +146,16 @@ class Columns(Environment):
         self.columns.append(c)
 
 
+class Fragment(_Fragment, CommonEnvironmentWithUtility):
+  @contextmanager
+  def frame(self, title=None):
+    with self.create(Frame(title=title)) as frame:
+      yield frame
+
+
 class CJK(Environment):
   _latex_name = "CJK*"
+
   def __init__(self, doc, *, options=None, **kwargs):
     super().__init__(arguments=["UTF8", "gbsn"], options=options, **kwargs)
     self.doc = doc
@@ -151,31 +166,34 @@ class CJK(Environment):
 
 class Beamer(object):
   """docstring for Beamer"""
+
   def __init__(self,
-      title,
-      subtitle=None,
-      author=None,
-      institute=None,
-      date=None,
-      outline_each_section=False,
-      outline_each_subsection=False,
-      page_number=True,
-      default_filepath=None,
-      theme="default",
-      color_theme="orchid",
-      inner_theme="rounded",
-      has_chinese=False,
-      font_theme=None,
-      main_font=None,
-      math_theme=None,
-      disable_pauses=False):
+               title,
+               subtitle=None,
+               author=None,
+               institute=None,
+               date=None,
+               outline_each_section=False,
+               outline_each_subsection=False,
+               page_number=True,
+               default_filepath=None,
+               theme="default",
+               color_theme="orchid",
+               inner_theme="rounded",
+               has_chinese=False,
+               font_theme=None,
+               main_font=None,
+               math_theme=None,
+               disable_pauses=False):
 
     options = []
     if disable_pauses:
       options.append("handout")
-    self.doc = Document(documentclass="beamer", document_options=options, default_filepath=default_filepath)
+    self.doc = Document(documentclass="beamer",
+                        document_options=options, default_filepath=default_filepath)
     if page_number:
-      self.doc.preamble.append(NoEscape(r"\setbeamertemplate{footline}[frame number]"))
+      self.doc.preamble.append(
+          NoEscape(r"\setbeamertemplate{footline}[frame number]"))
     self.doc.preamble.append(Command("usetheme", arguments=[theme]))
     self.doc.packages.append(Package("tikz"))
     self.doc.packages.append(Package("ulem"))
@@ -184,9 +202,11 @@ class Beamer(object):
     if has_chinese:
       self.doc.preamble.append(Package("CJKutf8"))
     if color_theme is not None:
-      self.doc.preamble.append(Command("usecolortheme", arguments=[color_theme]))
+      self.doc.preamble.append(
+          Command("usecolortheme", arguments=[color_theme]))
     if inner_theme is not None:
-      self.doc.preamble.append(Command("useinnertheme", arguments=[inner_theme]))
+      self.doc.preamble.append(
+          Command("useinnertheme", arguments=[inner_theme]))
     if font_theme is not None:
       self.doc.preamble.append(Command("usefonttheme", arguments=[font_theme]))
     if main_font is not None:
@@ -196,22 +216,37 @@ class Beamer(object):
         self.doc.packages.append(Package("fontspec"))
         self.doc.preamble.append(Command("setmainfont", arguments=[main_font]))
     if math_theme is not None:
-      self.doc.preamble.append(Command("usefonttheme", options=["onlymath"], arguments=[math_theme]))
-    self.doc.preamble.append(Command("definecolor", arguments=["olive", "rgb", "0.3, 0.4, .1"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["fore", "RGB", "249,242,215"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["back", "RGB", "51,51,51"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["title", "RGB", "255,0,90"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["dgreen", "rgb", "0.,0.6,0."]))
-    self.doc.preamble.append(Command("definecolor", arguments=["gold", "rgb", "1.,0.84,0."]))
-    self.doc.preamble.append(Command("definecolor", arguments=["JungleGreen", "cmyk", "0.99,0,0.52,0"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["BlueGreen", "cmyk", "0.85,0,0.33,0"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["RawSienna", "cmyk", "0,0.72,1,0.45"]))
-    self.doc.preamble.append(Command("definecolor", arguments=["Magenta", "cmyk", "0,1,0,0"]))
-    self.doc.preamble.append(Command("usetikzlibrary", arguments=["positioning"]))
+      self.doc.preamble.append(Command("usefonttheme", options=[
+                               "onlymath"], arguments=[math_theme]))
+    self.doc.preamble.append(Command("definecolor", arguments=[
+                             "olive", "rgb", "0.3, 0.4, .1"]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["fore", "RGB", "249,242,215"]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["back", "RGB", "51,51,51"]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["title", "RGB", "255,0,90"]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["dgreen", "rgb", "0.,0.6,0."]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["gold", "rgb", "1.,0.84,0."]))
+    self.doc.preamble.append(Command("definecolor", arguments=[
+                             "JungleGreen", "cmyk", "0.99,0,0.52,0"]))
+    self.doc.preamble.append(Command("definecolor", arguments=[
+                             "BlueGreen", "cmyk", "0.85,0,0.33,0"]))
+    self.doc.preamble.append(Command("definecolor", arguments=[
+                             "RawSienna", "cmyk", "0,0.72,1,0.45"]))
+    self.doc.preamble.append(
+        Command("definecolor", arguments=["Magenta", "cmyk", "0,1,0,0"]))
+    self.doc.preamble.append(
+        Command("usetikzlibrary", arguments=["positioning"]))
     self.doc.preamble.append(Command("usetikzlibrary", arguments=["shapes"]))
-    self.doc.preamble.append(Command("usetikzlibrary", arguments=["shapes.geometric"]))
-    self.doc.preamble.append(Command("usetikzlibrary", arguments=["decorations.pathreplacing"]))
-    self.doc.preamble.append(Command("usetikzlibrary", arguments=["decorations.text"]))
+    self.doc.preamble.append(
+        Command("usetikzlibrary", arguments=["shapes.geometric"]))
+    self.doc.preamble.append(
+        Command("usetikzlibrary", arguments=["decorations.pathreplacing"]))
+    self.doc.preamble.append(
+        Command("usetikzlibrary", arguments=["decorations.text"]))
     self.doc.preamble.append(NoEscape("""
 \\newcommand{\\blue}[1]{\\textcolor{blue}{#1}}
 \\newcommand{\\green}[1]{\\textcolor{green}{#1}}
@@ -248,9 +283,11 @@ class Beamer(object):
       self.doc.preamble.append(Command("author", arguments=[NoEscape(author)]))
     if institute is not None:
       if isinstance(institute, str):
-        self.doc.preamble.append(Command("institute", arguments=[NoEscape(institute)]))
+        self.doc.preamble.append(
+            Command("institute", arguments=[NoEscape(institute)]))
       elif isinstance(institute, list):
-        self.doc.preamble.append(Command("institute", arguments=[NoEscape(" \\and ".join(institute))]))
+        self.doc.preamble.append(Command("institute", arguments=[
+                                 NoEscape(" \\and ".join(institute))]))
     if date is not None:
       self.doc.preamble.append(Command("date", arguments=[date]))
     if has_chinese:
@@ -258,13 +295,13 @@ class Beamer(object):
         self.doc = cjk
 
   @contextmanager
-  def section(self, title):
-    with self.doc.create(Section(title)) as section:
+  def section(self, title, label=True):
+    with self.doc.create(Section(title, label=label)) as section:
       yield section
 
   @contextmanager
-  def subsection(self, title):
-    with self.doc.create(Subsection(title)) as subsection:
+  def subsection(self, title, label=True):
+    with self.doc.create(Subsection(title, label=label)) as subsection:
       yield subsection
 
   @contextmanager
